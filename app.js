abe3da23cd77aaee7c3ceef50347e49f3552bc37
linuser1990 <linuxserver@hotmail.com>
2023-12-01 21:07:49 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const pool = require('./db')
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+app.get('/cliente',async (req, res) =>
+{
+    const {rows} = await pool.query('SELECT * FROM cliente')
+    console.log(rows)
+    res.json({ results: rows });
+})
+
+
+app.get('/cidadesEstados', (req, res) =>
+{
+    res.render('dynamicTableCidadesEstados')
+})
+
+app.get('/', (req, res) =>
+{
+    res.render('dynamicTable')
+})
+
+app.listen(4000)
+
